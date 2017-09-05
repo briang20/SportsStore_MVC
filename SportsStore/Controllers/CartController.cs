de75be748416f278644622673bcf3dd0518f6e08
Brian Getz <briang846@gmail.com>
2017-09-05 16:23:48 -0400

    Middle of chapter 10

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -1,4 +1,7 @@
-﻿///<summary>
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+///<summary>
 /// There are a few points to note about this controller. The first is that I use the ASP.NET
 /// session state feature to store and retrieve Cart objects, which is the purpose of the GetCart
 /// method.The middleware that I registered in the previous section uses cookies or URL
@@ -10,28 +13,25 @@
 
 
 using System.Linq;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using SportsStore.Infrastructure;
-using SportsStore.Models;
-using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
 
         public ViewResult Index(string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
@@ -42,34 +42,23 @@ namespace SportsStore.Controllers
 
             if(product != null)
             {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
             }
+
             return RedirectToAction("Index", new { returnUrl });
         }
+
         public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
         {
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
 
             if(product != null)
             {
-                Cart cart = GetCart();
                 cart.RemoveLine(product);
-                SaveCart(cart);
             }
+
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
-        }
     }
 }
